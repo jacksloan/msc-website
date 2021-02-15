9bbccdbbc5ce60ab73f0f33b78cbd9c8e92c3933
Jack Sloan <jbsloan10@gmail.com>
2021-02-15 13:56:29 -0600

    add responsive mobal navigation

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
   import Nav from "../components/Nav.svelte";
   import Tailwindcss from "../components/Tailwindcss.svelte";
-  import { fly } from "svelte/transition";
-  import { expoInOut } from "svelte/easing";
+  import { fade } from "svelte/transition";
 
   export let segment: string;
 
@@ -21,16 +20,16 @@
 
 {#if show}
   <main
-    in:fly={{ y: 25, duration: 250, delay: 255, easing: expoInOut }}
-    out:fly={{ y: 75, duration: 250, easing: expoInOut }}
+    in:fade={{ delay: 205, duration: 200 }}
+    out:fade={{ duration: 200 }}
     class="relative max-w-4xl p-8 m-auto box-border"
   >
     <slot />
   </main>
 {:else}
   <main
-    in:fly={{ y: 25, duration: 250, delay: 255, easing: expoInOut }}
-    out:fly={{ y: 75, duration: 250, easing: expoInOut }}
+    in:fade={{ delay: 205, duration: 200 }}
+    out:fade={{ duration: 200 }}
     class="relative max-w-4xl p-8 m-auto box-border"
   >
     <slot />
