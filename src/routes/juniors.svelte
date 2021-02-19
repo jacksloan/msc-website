82fd533b0cdaab7a30f79327da403d54863f6c59
Jack Sloan <jbsloan10@gmail.com>
2021-02-18 21:01:43 -0600

    adding juniors page to netlify config

--- src/routes/juniors.svelte
+++ src/routes/juniors.svelte
@@ -6,15 +6,7 @@
   interface ProgramContent {
     tabName: string;
     content: string;
-    activities: Array<{
-      day?: string;
-      dateRange?: string;
-      ageGroup?: string;
-      time?: string;
-      location?: string;
-      content: string;
-      price?: string;
-    }>;
+    airtableLink?: string;
   }
 
   interface JuniorsPage {
@@ -28,95 +20,24 @@
     params,
     query,
   }): Promise<{ page: JuniorsPage }> {
-    try {
-      const res = await this.fetch("_content/juniors.json");
-      const json: JuniorsPage = await res.json();
-      return {
-        page: {
-          ...json,
-          content: marked(json.content),
-        },
-      };
-    } catch {
-      return {
-        page: {
-          title: "MSC Youth and Junior Programs",
-          seoTitle: "Juniors",
-          content: marked(""),
-          programs: [
-            {
-              tabName: "Spring",
-              content: marked("### Spring Stuff Goes Here"),
-              activities: [
-                {
-                  dateRange: "May 1 - June 1",
-                  ageGroup: "Youth/Junior",
-                  day: "Monday AM",
-                  content: "Technique and Distance",
-                  location: "Battle Creek",
-                  time: "7:00 AM - 9:00 AM",
-                },
-              ],
-            },
-            {
-              tabName: "Summer",
-              content: marked("### Summer Stuff Goes Here"),
-              activities: [
-                {
-                  dateRange: "June 1 - April 2",
-                  ageGroup: "Youth/Junior",
-                  day: "Monday AM",
-                  content: "Technique and Distance",
-                  location: "Battle Creek",
-                  time: "7:00 AM - 9:00 AM",
-                },
-                {
-                  dateRange: "June 1 - April 2",
-                  ageGroup: "Youth/Junior",
-                  day: "Monday AM",
-                  content: "Technique and Distance",
-                  location: "Battle Creek",
-                  time: "7:00 AM - 9:00 AM",
-                },
-                {
-                  dateRange: "June 1 - April 2",
-                  ageGroup: "Youth/Junior",
-                  day: "Monday AM",
-                  content: "Technique and Distance",
-                  location: "Battle Creek",
-                  time: "7:00 AM - 9:00 AM",
-                },
-                {
-                  dateRange: "June 1 - April 2",
-                  ageGroup: "Youth/Junior",
-                  day: "Monday AM",
-                  content: "Technique and Distance",
-                  location: "Battle Creek",
-                  time: "7:00 AM - 9:00 AM",
-                },
-              ],
-            },
-            {
-              tabName: "Fall-Winter",
-              content: marked("### Fall/Winter 2021-2022 Programming\n\nFollow [MSC on Facebook](https://www.facebook.com) to know when the fall and winter schedule is posted"),
-              activities: [],
-            },
-            {
-              tabName: "Race Program",
-              content: marked("### Advanced Racing Program (Year Round)"),
-              activities: [],
-            },
-          ],
-        },
-      };
-    }
+    const res = await this.fetch("_content/juniors.json");
+    const json: JuniorsPage = await res.json();
+    return {
+      page: {
+        ...json,
+        content: marked(json.content),
+        programs: json.programs.map((p) => ({
+          ...p,
+          content: marked(p.content),
+        })),
+      },
+    };
   }
 </script>
 
 <script lang="ts">
   import { fly } from "svelte/transition";
   import NavTabs from "../components/NavTabs.svelte";
-  import ProgramCard from "../components/ProgramCard.svelte";
 
   export let page: JuniorsPage;
 
@@ -136,6 +57,8 @@
     activities: [],
     content: "",
   };
+
+  $: selectedHasAirtableLink = !!selectedProgramContent?.airtableLink;
 
   let show = true;
 
@@ -163,20 +86,19 @@
     in:fly={{ y: 25, delay: 205, duration: 200 }}
     out:fly={{ y: 75, duration: 200 }}
   >
-    {@html selectedProgramContent.content}
-
-    <div class="flex flex-col flex-wrap md:gap-7 md:flex-row">
-      {#each selectedProgramContent.activities as activity}
-        <ProgramCard
-          location={activity.location}
-          title={activity.day}
-          ageGroup={activity.ageGroup}
-          date={activity.dateRange}
-          time={activity.time}
-          content="Strength, Technique, and Distance"
-        />
-      {/each}
-    </div>
+    {@html selectedProgramContent.content || ""}
+    {#if selectedHasAirtableLink}
+      <h3>{selectedProgramContent.tabName} Schedule</h3>
+      <iframe
+        title={selectedProgramContent.tabName + " schedule"}
+        class="airtable-embed"
+        src={selectedProgramContent.airtableLink}
+        frameborder="0"
+        width="100%"
+        height="533"
+        style="background: transparent; border: 1px solid #ccc;"
+      />
+    {/if}
   </div>
 {:else}
   <div
@@ -184,18 +106,17 @@
     out:fly={{ y: 75, duration: 200 }}
   >
     {@html selectedProgramContent.content || ""}
-
-    <div class="flex flex-col md:flex-row">
-      {#each selectedProgramContent.activities || [] as activity}
-        <ProgramCard
-          location={activity.location}
-          title={activity.day}
-          ageGroup={activity.ageGroup}
-          date={activity.dateRange}
-          time={activity.time}
-          content="Strength, Technique, and Distance"
-        />
-      {/each}
-    </div>
+    {#if selectedHasAirtableLink}
+      <h3>{selectedProgramContent.tabName} Schedule</h3>
+      <iframe
+        title={selectedProgramContent.tabName + " schedule"}
+        class="airtable-embed"
+        src={selectedProgramContent.airtableLink}
+        frameborder="0"
+        width="100%"
+        height="600"
+        style="background: transparent; border: 1px solid #ccc;"
+      />
+    {/if}
   </div>
 {/if}
