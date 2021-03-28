6eb8f6154f37a2e35f27b577b1f256034cc86984
Jack Sloan <jbsloan10@gmail.com>
2021-03-28 13:59:02 -0500

    data-netlify="true" must exist on the form element

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -62,9 +62,9 @@
   </a>
 </section>
 
-<section aria-label="Contact Form" data-netlify="true">
+<section aria-label="Contact Form">
   <h4>Or send us a message</h4>
-  <form class="w-full max-w-lg" name="contact">
+  <form class="w-full max-w-lg" name="contact" data-netlify="true">
     <div class="flex flex-wrap -mx-3 mb-6">
       <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
         <label
