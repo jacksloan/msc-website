7614dd7b0b5890cfe0f8413eddd49081db30f0ae
Jack Sloan <jbsloan10@gmail.com>
2021-04-11 13:38:15 -0500

    migrate adults page to sveltekit

---
+++ src/routes/adults.svelte
@@ -0,0 +1,83 @@
+<script context="module" lang="ts">
+	import marked from 'marked';
+
+	interface ProgramContent {
+		tabName: string;
+		content: string;
+		airtableLink?: string;
+	}
+
+	interface AdultsPage {
+		title: string;
+		seoTitle: string;
+		content: string;
+		programs: ProgramContent[];
+	}
+
+	export async function load({ fetch }): Promise<{ props: { page: AdultsPage } }> {
+		const res = await fetch('_content/adults.json');
+		const json: AdultsPage = await res.json();
+		return {
+			props: {
+				page: {
+					...json,
+					content: marked(json.content),
+					programs: json.programs.map((p) => ({
+						...p,
+						content: marked(p.content)
+					}))
+				}
+			}
+		};
+	}
+</script>
+
+<script lang="ts">
+	import NavTabs from '$lib/NavTabs.svelte';
+
+	export let page: AdultsPage;
+
+	let tabs = {
+		tabs: page.programs.map((p) => p.tabName),
+		initialTab: 0
+	};
+
+	let selectedTab: string = '';
+
+	let selectedProgramContent: ProgramContent;
+
+	$: selectedProgramContent = page.programs.find((it) => it.tabName === selectedTab) || {
+		tabName: '',
+		activities: [],
+		content: ''
+	};
+
+	$: selectedHasAirtableLink = !!selectedProgramContent?.airtableLink;
+</script>
+
+<svelte:head>
+	<title>MSC - {page.seoTitle}</title>
+	<meta name="description" content={'MSC - ' + page.seoTitle} />
+</svelte:head>
+
+<h1 class="page-title">{page.title}</h1>
+
+<div class="markdown-content">
+	{@html page.content}
+</div>
+
+<NavTabs {...tabs} bind:selectedTab />
+
+{@html selectedProgramContent.content || ''}
+{#if selectedHasAirtableLink}
+	<h3>{selectedProgramContent.tabName} Schedule</h3>
+	<iframe
+		title={selectedProgramContent.tabName + ' schedule'}
+		class="airtable-embed"
+		src={selectedProgramContent.airtableLink}
+		frameborder="0"
+		width="100%"
+		height="533"
+		style="background: transparent; border: 1px solid #ccc;"
+	/>
+{/if}
